a141264ad2749789187a804c2e80f67debef3038
SRINIVAS PALAKALA <SPALAKALA@MICRON.COM>
2016-09-12 15:27:59 -0600

    step 1

--- src/app.js
+++ src/app.js
@@ -1,60 +1,10 @@
 angular
-  .module('app', [])
-  .controller('AppController', AppController);
+  .module('app', ['hero'])
+  .controller('AppController', AppController); 
 
-
-function AppController($scope) {
-
-  $scope.title = 'List Page';
-  
-  $scope.heroes = [
-    {
-      id: 0,
-      name: 'Luke Skywalker',
-      imageUrl: 'images/luke.png',
-      power: 9000,
-      affiliations: ['Jedi', 'Rebel'],
-      light: true
-    },
-    {
-      id: 1,
-      name: 'R2D2',
-      imageUrl: 'images/r2d2.png',
-      power: 2000,
-      affiliations: ['Droid', 'Rebel'],
-      light: true
-    },
-    {
-      id: 2,
-      name: 'Chewie',
-      imageUrl: 'images/chewie.png',
-      power: 6000,
-      affiliations: ['Scoundrel', 'Rebel'],
-      light: true
-    },
-    {
-      id: 3,
-      name: 'Darth Maul',
-      imageUrl: 'images/darthMaul.png',
-      power: 8000,
-      affiliations: ['Sith'],
-      light: false
-    },
-    {
-      id: 4,
-      name: 'Darth Vader',
-      imageUrl: 'images/darthVader.png',
-      power: 10000,
-      affiliations: ['Sith', 'Empire'],
-      light: false
-    },
-    {
-      id: 5,
-      name: 'Jango Fett',
-      imageUrl: 'images/jangoFett.png',
-      power: 7000,
-      affiliations: ['Bounty Hunter', 'Mandalorian'],
-      light: false
-    },
-  ]
-}
+function AppController ($scope, heroService) {
+    //This could have been anony call to AppController function which would be implmented as AppController($scope, heroService)
+    //Jeremy says better to avoid anonymous here because you could add more nested calls like .filter on the controller. 
+    $scope.heroes = heroService.getAll();
+    $scope.title = "Micron Heroes";
+  }
